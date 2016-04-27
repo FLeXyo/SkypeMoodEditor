8cedfd2d123d4bf73a619eb6d24c0d0eedd9c4a1
FLeX <ahjflex@gmail.com>
2016-04-27 20:52:49 +0200

    Add project files.

---
+++ SkypeMoodEditor/Program.cs
@@ -0,0 +1,25 @@
+﻿using SKYPE4COMLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SkypeMoodEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Skype s = new Skype();
+            while (true)
+            {
+                Console.Write("Enter new mood message with tags (usable tags: <i>, <b>, <u>, <font>, <center>, <blink>, <a>): ");
+                string msg = Console.ReadLine();
+                s.CurrentUserProfile.MoodText = Regex.Replace(msg, @" <.*?> ", ""); //Remove tags";
+                s.CurrentUserProfile.RichMoodText = msg;
+            }
+        }
+    }
+}
